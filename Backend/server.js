5fc34ed2147a33cb09ade97d3b67dfa262ae5425
DeadAnjali <anjalimahere15@gmail.com>
2024-03-18 15:16:35 +0530

    update-server

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,13 @@ const dbConnect=require("./config/db");
 const userRoute = require("./route/user/userRoute");
 dbConnect();
 
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your frontend origin
+    optionsSuccessStatus: 200 // Set to 200 to avoid issues with legacy browsers
+}));
+
+
 app.use(express.json());//this is a middleware used to communicate between request and response
 app.use("/api/users",userRoute);
 
@@ -17,3 +24,4 @@ app.use(notFound);
 app.use(errorHandler);
 const PORT=process.env.PORT||5000;
 app.listen(PORT,console.log(`the port ${PORT}is running`));
+
