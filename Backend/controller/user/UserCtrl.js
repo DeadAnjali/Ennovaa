a604445c5d7dd6a847846de1256f1eba0b16ac62
DeadAnjali <anjalimahere15@gmail.com>
2024-03-18 14:11:47 +0530

    error-handler

--- Backend/controller/user/UserCtrl.js
+++ Backend/controller/user/UserCtrl.js
@@ -3,9 +3,9 @@ const User = require("../../model/user/User");
 const expressAsyncHandler=require("express-async-handler");
 const validMongoDbId = require("../utils/validateMongoDB");
 
-const UserRegusterCtrl=async (req,res)=>{
-    // const userExist= await User.findOne({email:req?.body?.email});
-    //if(userExist) throw new Error("Already in use");
+const UserRegusterCtrl=expressAsyncHandler(async (req,res)=>{
+    const userExist= await User.findOne({email:req?.body?.email});
+    if(userExist) throw new Error("Already in use");
 
     //throw error in controller
     try {
@@ -19,9 +19,8 @@ const UserRegusterCtrl=async (req,res)=>{
     } catch (error) {
         res.json({error})
     }
-    
-}
-const UserLoginCtrl=async (req,res)=>{
+});
+const UserLoginCtrl=expressAsyncHandler(async (req,res)=>{
     const {email,password}=req.body;
     const userLogin= await User.findOne({email});
     if(userLogin&& await (userLogin.isPasswordMatched(password))){
@@ -36,7 +35,7 @@ const UserLoginCtrl=async (req,res)=>{
         //throw new Error(`Login credentials are not valid`);
         res.json({message:"Your login is not valid"});
     }
-}
+});
 const fetchUsersCtrl=expressAsyncHandler(async(req,res)=>{
     try{
         const users=await User.find({});
--- Backend/middlewares/error/errorHandler.js
+++ Backend/middlewares/error/errorHandler.js
@@ -1,21 +1,19 @@
-//this middle ware is used with async error handler which is used in the error handling video
-
-const notFound=(req,res,next)=>{
-    const err=new Error(`Not Found -${req.originalUrl}`);
+//Not found
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
-}
-
-const errorHandler=(err,req,res,next)=>{
-    const statusCode=res.statusCode===200?500:res.statusCode;
-    res.status(statusbar);
+  };
+  
+  //Err handler
+  const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
     res.json({
-        message:err?.message,
-        stack:process.env.NODE_ENV==='production'?null:err.stack,
-    })
-}
-
-module.exports=errorHandler;
-module.exports=notFound;
-
-//import it in server file after the controllers
+      message: err?.message,
+      stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+  };
+  
+  module.exports = { errorHandler, notFound };
+  
